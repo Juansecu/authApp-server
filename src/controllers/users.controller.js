6142dbf74a83e38d346c2dd92cc92512d9e90b1a
Juansecu <juansecoder@hotmail.com>
2021-08-04 15:27:36 -0500

    Registration for new users was configured

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,12 +1,43 @@
 const { request, response } = require('express');
-const { validationResult } = require('express-validator');
+const { hashSync } = require('bcryptjs');
+
+const User = require('../models/User');
 
 /**
  * @param {request} req
  * @param {response} res
  */
-module.exports.addUser = (req, res) => {
-    res.send('Successfully registered!');
+module.exports.addUser = async (req, res) => {
+    const { email, firstName, lastName, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user)
+            return res.status(409).json({
+                ok: false,
+                message: 'User already exists!'
+            });
+
+        user = new User({ email, firstName, lastName, password });
+        user.password = hashSync(password, 10);
+
+        await user.save();
+
+        return res.status(201).json({
+            ok: true,
+            userId: user.id,
+            firstName,
+            lastName
+        });
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            ok: false,
+            error: 'Internal server error. Please contact with the administrator.'
+        });
+    }
 };
 
 /**

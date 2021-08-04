99e65ed623a3762b34fb4324246c11da6a60fd32
Juansecu <juansecoder@hotmail.com>
2021-08-04 08:20:20 -0500

    Cors and json from express were setted as middlewares

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(cors());
+app.use(express.json());
 
 app.use('/api', routes);
 

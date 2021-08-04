f0fc1465d7569b565bcd1b53c39fa7c71d1ec8d9
Juansecu <juansecoder@hotmail.com>
2021-08-04 14:35:55 -0500

    User schema was added

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = model('User', UserSchema);
